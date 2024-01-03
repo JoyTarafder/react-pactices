9ae2ba50872c96c1812217819dabc18538e651b5
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-01-04 00:28:35 +0600

    Add password copy functionality

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState, useCallback, useEffect } from 'react'
+// Desc: This file contains the main component of the application
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
+  // useState is used to create a state variable and a function to update it
   const [password, setPassword] = useState('')
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
+
+  // useRef is used to create a reference to an element
+  const passwordRef = useRef(null)
 
   // useCallback is used to prevent the function from being re-created every time the component is re-rendered
 
@@ -24,28 +29,44 @@ function App() {
     setPassword(pass)
   },[length, numberAllowed, charAllowed,setPassword])
 
-// useEffect is used to run the function when the component is mounted and when the dependencies change
+  const copyPasswordClipBoard = useCallback(() => { 
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 100);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+
+  // useEffect is used to run the function when the component is mounted and when the dependencies change
+  
   useEffect(() => { 
     generatePassword()
-  },[length, numberAllowed, charAllowed, generatePassword])
+  }, [length, numberAllowed, charAllowed, generatePassword])
+
+  // The return statement is used to return the JSX
   return (
-    <div>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 text-orange-500 bg-gray-700'>
+    // The className attribute is used to add classes to the elements
+    // The tailwindcss classes are used to style the elements
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-9 text-orange-500 bg-gray-700'>
         <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-        <br />
+      <br />
+      
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input
           type="text"
           value={password}
           placeholder='Your password'
           readOnly
+          ref={passwordRef}
           className='outline-none w-full py-1 px-3'
-          />
+        />
           <button
-          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-indigo-500 hover:text-slate-950'
+          onClick={copyPasswordClipBoard}
+
           > copy
-          </button>
-        </div>
+        </button>
+      </div>
+      
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input type="range" min={6} max={100} value={length}
@@ -72,7 +93,6 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
